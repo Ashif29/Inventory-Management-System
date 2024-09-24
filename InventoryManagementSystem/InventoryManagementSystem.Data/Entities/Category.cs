2e7cb683733eebbe14312907c57d28e87601f10a
Asif <asif.duet.cse19@gmail.com>
2024-09-24 20:17:21 +0600

    Added Category Model

---
+++ InventoryManagementSystem/InventoryManagementSystem.Data/Entities/Category.cs
@@ -0,0 +1,22 @@
+﻿using InventoryManagementSystem.Data.Entities.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Data.Entities
+{
+    public class Category : BaseEntity
+    {
+        [Required(ErrorMessage = "Category name is required")]
+        [StringLength(100, ErrorMessage = "The name cannot exceed 100 characters.")]
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        // Navigation Property
+        public  ICollection<Product> Products { get; set; }
+    }
+}
