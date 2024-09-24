dd888194854f574a2f24d5c258711d46d264e6e2
Asif <asif.duet.cse19@gmail.com>
2024-09-24 20:17:41 +0600

    Added BaseEntity

---
+++ InventoryManagementSystem/InventoryManagementSystem.Data/Entities/Core/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Data.Entities.Core
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.Now;
+        }
+    }
+}
