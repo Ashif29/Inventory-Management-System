e03189afb0ecb966e363fbced0d99148980cfbc2
Asif <asif.duet.cse19@gmail.com>
2024-09-24 20:31:59 +0600

    Added ApplicationDbContextAndMigrations

--- InventoryManagementSystem/InventoryManagementSystem.Web/Program.cs
+++ InventoryManagementSystem/InventoryManagementSystem.Web/Program.cs
@@ -1,6 +1,16 @@
+using InventoryManagementSystem.Data.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSqlConnection"));
+});
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
