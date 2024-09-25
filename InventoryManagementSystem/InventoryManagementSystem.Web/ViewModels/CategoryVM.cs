b4d80619ea2b6f0d8b0503218ebcd226c6ddfe29
Asif <asif.duet.cse19@gmail.com>
2024-09-25 22:53:11 +0600

    Added CategoryVM

---
+++ InventoryManagementSystem/InventoryManagementSystem.Web/ViewModels/CategoryVM.cs
@@ -0,0 +1,13 @@
+﻿using InventoryManagementSystem.Data.Entities.NotMapped;
+using InventoryManagementSystem.Data.Entities;
+
+namespace InventoryManagementSystem.Web.ViewModels
+{
+    public class CategoryVM
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public CategoryQueryParameters? QueryParameters { get; set; }
+
+        public PaginatedList<Category> CategoriesPaged { get; set; }
+    }
+}
