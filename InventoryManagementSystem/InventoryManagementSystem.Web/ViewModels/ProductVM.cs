dedb0cd1fa1f18535b679e7994877022933de14f
Asif <asif.duet.cse19@gmail.com>
2024-09-25 20:03:30 +0600

    Added Product ViewModel

---
+++ InventoryManagementSystem/InventoryManagementSystem.Web/ViewModels/ProductVM.cs
@@ -0,0 +1,13 @@
+﻿using InventoryManagementSystem.Data.Entities;
+using InventoryManagementSystem.Data.Entities.NotMapped;
+
+namespace InventoryManagementSystem.Web.ViewModels
+{
+    public class ProductVM
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public ProductQueryParameters? QueryParameters { get; set; }
+
+        public PaginatedList<Product> ProductsPaged { get; set; }
+    }
+}
