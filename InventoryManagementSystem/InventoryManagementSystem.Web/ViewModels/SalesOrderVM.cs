11475da75b9fe64a18c9909a872d0266ca21876c
Asif <asif.duet.cse19@gmail.com>
2024-09-28 14:18:17 +0600

    Added SalesOrderViewModel

---
+++ InventoryManagementSystem/InventoryManagementSystem.Web/ViewModels/SalesOrderVM.cs
@@ -0,0 +1,30 @@
+﻿using InventoryManagementSystem.Data.Entities;
+using InventoryManagementSystem.Data.Entities.NotMapped;
+using InventoryManagementSystem.Data.Enums;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace InventoryManagementSystem.Web.ViewModels
+{
+    public class SalesOrderVM
+    {
+        public string CurrentSalesmanName { get; set; }
+
+        public DateTime CurrentDate = DateTime.Now;
+
+        public string nextSOCode { get; set; }
+        public SalesOrder SalesOrder { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<SalesOrder> SalesOrderList { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem> ConsumersList { get; set; }
+        public List<SalesOrderDetailItem> SalesOrderDetailItems { get; set; }
+
+        public class SalesOrderDetailItem : SalesOrderDetail
+        {
+
+        }
+    }
+}
